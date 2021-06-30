303e3a8944fa42b799f67a17b8188082af7a27ce
amalmtahri <mtahriamal0@gmail.com>
2021-06-30 18:42:12 +0100

    change

--- wp-content/plugins/contact_plugin/affichage.php
+++ wp-content/plugins/contact_plugin/affichage.php
@@ -46,8 +46,9 @@
             $requette = "SELECT * FROM contactus order by id desc "; 
             $result = mysqli_query($connection, $requette);
             while ($row = $result->fetch_assoc()) {
-                echo '<tr ><td>'. $row["fullname"] . '</td><td name="email">' . $row["email"] . '</td><td>' . $row["subjecte"] . '</td><td>' . $row["content"] . '</td>'.'<td><input class="btn btn-primary" onclick="poppup()" type="submit" value="Repondre"  /></td></tr>';
+                echo '<tr ><td>'. $row["fullname"] . '</td><td name="email">' . $row["email"] . '</td><td>' . $row["subjecte"] . '</td><td>' . $row["content"] . '</td>'.'<td><input class="btn btn-primary" onclick="poppup(`'.$row["email"].'`)" type="submit" value="Repondre"/>&nbsp;<a href="./admin.php?page=contact-dashbord&id='.$row['id'].'" class="btn  btn-danger">'."Supprimer".'</a></td></tr>';
             }
+            
     ?>
   </tbody>
 </table>
@@ -55,7 +56,7 @@
 <h1 class="text-center text-white">Repondre</h1>
 <p class=" btn btn-outline-light" onclick="function2()">x</p>
 <form action="" method="post">
-<input type="hidden" value="mtahramal13@gmail.com" name="emailuser">
+<input type="text" name="emailuser" class="emailuser">
 <input type="text" class="form-control" name="message"><br/>
 <button class="btn btn-outline-light" name="repondre">Rependre</button>
 </form>
@@ -67,12 +68,25 @@ if(isset($_POST['repondre'])){
     $message = $_POST['message'];
     wp_mail($to,$subject,$message);
 }
+if(isset($_GET['id']) && !empty($_GET['id']))
+{
+    $connection = mysqli_connect('localhost','root','');
+    mysqli_select_db($connection,"wordpress2");
+    $query = "DELETE FROM `contactus` WHERE id=".$_GET['id'];
+    mysqli_query($connection, $query);
+    global $wp;
+    $urlv=add_query_arg( $wp->query_vars, home_url() );
+      
+}
+
 ?>
 
 <script>
-        function poppup(){
+        function poppup(x){
             var message=document.getElementById("message");
             message.style="display:block";
+            document.querySelector('.emailuser').value=x;
+
         }
         function function2(){
             var message=document.getElementById("message");
